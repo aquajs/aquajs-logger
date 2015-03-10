c88b046d7907048a6535f5e50bb98dd964de53c6
umore <umore@equinix.com>
2015-03-10 00:29:20 -0700

    Added logger id to each log line for Console, File and DailyRotateFile

--- index.js
+++ index.js
@@ -60,8 +60,10 @@ ConsoleLogger.prototype.log = function (level, msg, meta, callback) {
   callback(null, true);
 };
 
+var loggerIndex = null;
 
 var AquaJsLogger = function () {
+  loggerIndex = 1;
   console.log("Logger created");
 };
 
@@ -87,140 +89,127 @@ var AquaJsLogger = function () {
  */
 
 AquaJsLogger.prototype.init = function(configArgs) {
-    var logConfig = configArgs.logConfig,
-        logger = new winston.Logger(),
-        fileCfg;
-    Object.keys(logConfig).forEach(function(key) {
-        var transCfg = logConfig[key];
-        switch (key) {
-          case "console":
-            logger.add(winston.transports.Console, {
-              colorize: transCfg.colorize || "true",
-              timestamp: transCfg.timestamp || "true",
-              level: transCfg.level || "info",
-              handleExceptions: true,
-              timestamp: function () {
-                now = new Date();
-                year = "" + now.getFullYear();
-                month = "" + (now.getMonth() + 1);
-                if (month.length == 1) { month = "0" + month; }
-                day = "" + now.getDate();
-                if (day.length == 1) { day = "0" + day; }
-                hour = "" + now.getHours();
-                if (hour.length == 1) { hour = "0" + hour; }
-                minute = "" + now.getMinutes();
-                if (minute.length == 1) { minute = "0" + minute; }
-                second = "" + now.getSeconds();
-                if (second.length == 1) { second = "0" + second; }
-                milliseconds = "" + now.getMilliseconds();
-                if (milliseconds.length == 1) { milliseconds = "0" + milliseconds; }
-                return month + "-" + day + "-" + year + " " + hour + ":" + minute + ":" + second + ":" + milliseconds;
-              }
-            });
-            break;
-          case "file":
-            fileCfg = {
-              filename: transCfg.filename || "application.log",
-              handleExceptions: transCfg.handleExceptions || true,
-              exitOnError: transCfg.exitOnError || false,
-              json: false,
-              level: transCfg.level || "info",
-              timestamp: transCfg.timestampFormat || function () {
-                now = new Date();
-                year = "" + now.getFullYear();
-                month = "" + (now.getMonth() + 1);
-                if (month.length == 1) { month = "0" + month; }
-                day = "" + now.getDate();
-                if (day.length == 1) { day = "0" + day; }
-                hour = "" + now.getHours();
-                if (hour.length == 1) { hour = "0" + hour; }
-                minute = "" + now.getMinutes();
-                if (minute.length == 1) { minute = "0" + minute; }
-                second = "" + now.getSeconds();
-                if (second.length == 1) { second = "0" + second; }
-                milliseconds = "" + now.getMilliseconds();
-                if (milliseconds.length == 1) { milliseconds = "0" + milliseconds; }
-                return month + "-" + day + "-" + year + " " + hour + ":" + minute + ":" + second + ":" + milliseconds;
-              }
+  var logConfig = configArgs.logConfig,
+      logger = new winston.Logger(),
+      fileCfg;
+
+  logger.on('logging', function (transport, level, msg, meta) {
+    loggerIndex ++;
+  });
+
+  Object.keys(logConfig).forEach(function(key) {
+    var transCfg = logConfig[key];
+    switch (key) {
+      case "console":
+        logger.add(winston.transports.Console, {
+          colorize: transCfg.colorize || true,
+          timestamp: transCfg.timestamp || true,
+          level: transCfg.level || "info",
+          handleExceptions: true,
+          timestamp:  transCfg.timestamp || getCustomTimeStamp,
+          formatter: function(options) {
+            // Return string will be passed to logger.
+            return options.timestamp() + ' microservice-' + loggerIndex + ' '
+                + options.level.toUpperCase() + ' '
+                + (undefined !== options.message ? options.message : '')
+                + (options.meta && Object.keys(options.meta).length ? '\n\t'
+                + JSON.stringify(options.meta) : '');
+          }
+        });
+        break;
+      case "file":
+        fileCfg = {
+          filename: transCfg.filename || "application.log",
+          handleExceptions: transCfg.handleExceptions || true,
+          exitOnError: transCfg.exitOnError || false,
+          json: false,
+          level: transCfg.level || "info",
+          timestamp:  transCfg.timestamp || getCustomTimeStamp,
+          formatter: function(options) {
+            // Return string will be passed to logger.
+            return options.timestamp() + ' microservice-' + loggerIndex + ' '
+                + options.level.toUpperCase() + ' '
+                + (undefined !== options.message ? options.message : '')
+                + (options.meta && Object.keys(options.meta).length ? '\n\t'
+                + JSON.stringify(options.meta) : '');
+          }
+        };
+        logger.add(winston.transports.File, fileCfg);
+        break;
+      case "rollingFile":
+        fileCfg = {
+          filename: transCfg.filename || "application.log",
+          name: transCfg.name || "rollingFileAppender",
+          handleExceptions: transCfg.handleExceptions || true,
+          exitOnError: transCfg.exitOnError || false,
+          level: transCfg.level || "info",
+          json: false,
+          datePattern: transCfg.datePattern || '.yyyy-MM-ddTHH',
+          timestamp:  transCfg.timestamp || getCustomTimeStamp,
+          formatter: function(options) {
+            // Return string will be passed to logger.
+            return options.timestamp() + ' microservice-' + loggerIndex + ' '
+                + options.level.toUpperCase() + ' '
+                + (undefined !== options.message ? options.message : '')
+                + (options.meta && Object.keys(options.meta).length ? '\n\t'
+                + JSON.stringify(options.meta) : '');
+          }
+        };
+        logger.add(winston.transports.DailyRotateFile, fileCfg);
+        break;
+      case "email":
+        var Mail = require('winston-mail').Mail,
+            emailCfg = {
+              to: transCfg.to || "aqaujsadmin@equninix.com",
+              from: transCfg.from || "aqaujsadmin@equninix.com",
+              host: transCfg.host,
+              port: transCfg.port,
+              username: transCfg.username,
+              password: transCfg.password,
+              ssl: transCfg.ssl,
+              tls: transCfg.tls,
+              level: transCfg.level || "error",
+              silent: transCfg.silent || true
             };
-            logger.add(winston.transports.File, fileCfg);
-            winston.handleExceptions(new winston.transports.File(fileCfg));
-            break;
-          case "rollingFile":
-            fileCfg = {
-              filename: transCfg.filename || "application.log",
-              name: transCfg.name || "rollingFileAppender",
-              handleExceptions: transCfg.handleExceptions || true,
-              exitOnError: transCfg.exitOnError || false,
-              level: transCfg.level || "info",
-              json: false,
-              datePattern: transCfg.datePattern || '.yyyy-MM-ddTHH',
-              timestamp: transCfg.timestampFormat || function () {
-                now = new Date();
-                year = "" + now.getFullYear();
-                month = "" + (now.getMonth() + 1);
-                if (month.length == 1) { month = "0" + month; }
-                day = "" + now.getDate();
-                if (day.length == 1) { day = "0" + day; }
-                hour = "" + now.getHours();
-                if (hour.length == 1) { hour = "0" + hour; }
-                minute = "" + now.getMinutes();
-                if (minute.length == 1) { minute = "0" + minute; }
-                second = "" + now.getSeconds();
-                if (second.length == 1) { second = "0" + second; }
-                milliseconds = "" + now.getMilliseconds();
-                if (milliseconds.length == 1) { milliseconds = "0" + milliseconds; }
-                return month + "-" + day + "-" + year + " " + hour + ":" + minute + ":" + second + ":" + milliseconds;
-              }
-            };
-            logger.add(winston.transports.DailyRotateFile, fileCfg);
-            winston.handleExceptions(new winston.transports.DailyRotateFile(fileCfg));
-            break;
-            case "email":
-                var Mail = require('winston-mail').Mail,
-                    emailCfg = {
-                        to: transCfg.to || "aqaujsadmin@equninix.com",
-                        from: transCfg.from || "aqaujsadmin@equninix.com",
-                        host: transCfg.host,
-                        port: transCfg.port,
-                        username: transCfg.username,
-                        password: transCfg.password,
-                        ssl: transCfg.ssl,
-                        tls: transCfg.tls,
-                        level: transCfg.level || "error",
-                        silent: transCfg.silent || true
-                    };
-                winston.add(Mail, emailCfg);
-                winston.handleExceptions( new Mail(emailCfg));
-                break;
-            case "mongodb":
-                var MongoDB = require('winston-mongodb').MongoDB;
-                winston.add(MongoDB, {
-                    level: transCfg.level || "info",
-                    silent: transCfg.silent || true,
-                    db: transCfg.db,
-                    collection: transCfg.collection,
-                    safe: transCfg.safe,
-                    host: transCfg.host,
-                    port: transCfg.port
-                });
-                break;
-            case "Cassandra":
-                var Cassandra = require('winston-cassandra').Cassandra;
-                winston.add(Cassandra, {
-                    level: transCfg.level,
-                    table: transCfg.table,
-                    partitionBy: transCfg.partitionBy,
-                    consistency: transCfg.consistency,
-                    hosts: transCfg.hosts,
-                    keyspace: transCfg.keyspace
-                });
-                break;
-            default:
-        }
-    });
-    this.winston = winston;
-    this.logger = logger;
+        winston.add(Mail, emailCfg);
+        break;
+      case "mongodb":
+        var MongoDB = require('winston-mongodb').MongoDB;
+        winston.add(MongoDB, {
+          level: transCfg.level || "info",
+          silent: transCfg.silent || true,
+          db: transCfg.db,
+          collection: transCfg.collection,
+          safe: transCfg.safe,
+          host: transCfg.host,
+          port: transCfg.port
+        });
+        break;
+      case "cassandra":
+        var Cassandra = require('winston-cassandra').Cassandra;
+        winston.add(Cassandra, {
+          level: transCfg.level,
+          table: transCfg.table,
+          partitionBy: transCfg.partitionBy,
+          consistency: transCfg.consistency,
+          hosts: transCfg.hosts,
+          keyspace: transCfg.keyspace
+        });
+        break;
+      case "logIo":
+        var LogIo = require('winston-logio');
+        logger.add(LogIo.Logio,  {
+          level: 'info',
+          port: transCfg.port|| 28777,
+          node_name: transCfg.node_name,
+          host: transCfg.host
+        });
+        break;
+      default:
+    }
+  });
+  this.winston = winston;
+  this.logger = logger;
 
 };
 
@@ -240,6 +229,24 @@ AquaJsLogger.prototype.getWinston = function () {
 AquaJsLogger.prototype.getLogger = function () {
   // will be undefined if init not called first
   return this.logger;
+};
+
+var getCustomTimeStamp = function (){
+  now = new Date();
+  year = "" + now.getFullYear();
+  month = "" + (now.getMonth() + 1);
+  if (month.length == 1) { month = "0" + month; }
+  day = "" + now.getDate();
+  if (day.length == 1) { day = "0" + day; }
+  hour = "" + now.getHours();
+  if (hour.length == 1) { hour = "0" + hour; }
+  minute = "" + now.getMinutes();
+  if (minute.length == 1) { minute = "0" + minute; }
+  second = "" + now.getSeconds();
+  if (second.length == 1) { second = "0" + second; }
+  milliseconds = "" + now.getMilliseconds();
+  if (milliseconds.length == 1) { milliseconds = "0" + milliseconds; }
+  return month + "-" + day + "-" + year + " " + hour + ":" + minute + ":" + second + ":" + milliseconds;
 };
 
 //Export the singleton AquaJSLogger Instance which will be used through require('AquaJSLogger')
